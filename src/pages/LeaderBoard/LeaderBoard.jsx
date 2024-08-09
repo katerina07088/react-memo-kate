40de09d4147cec7d102cfff5d5a074d60a8dc826
katerina07088 <ekaterinakorotenko@yandex.ru>
2024-08-09 16:46:56 +0300

    got the listOfLeaders

--- src/pages/LeaderBoard/LeaderBoard.jsx
+++ src/pages/LeaderBoard/LeaderBoard.jsx
@@ -1,21 +1,22 @@
-//import { useEffect } from "react";
+import { useEffect } from "react";
 import { Button } from "../../components/Button/Button";
 import styles from "./LeaderBoard.module.css";
-//import { getListOfLeaders } from "../../api/apiLeaderBoard";
-//import { useState } from "react";
+import { getListOfLeaders } from "../../api/api";
+import { useState } from "react";
 
 export function LeaderBoard({ onClick }) {
-  //const [error, setError] = useState("");
+  const [error, setError] = useState("");
+  const [leaders, setListOfLeaders] = useState([]);
 
-  //useEffect(() => {
-  //getListOfLeaders()
-  //.then(res => {
-  //setTasks(res.tasks);
-  //})
-  //.catch(error => {
-  //setError(error.message);
-  //});
-  //}, []);
+  useEffect(() => {
+    getListOfLeaders()
+      .then(res => {
+        setListOfLeaders(res.leaders);
+      })
+      .catch(error => {
+        setError(error.message);
+      });
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -27,14 +28,18 @@ export function LeaderBoard({ onClick }) {
       <div className={styles.leadersList}>
         <ul className={styles.leaders}>
           <li className={styles.leader}>
-            <div className={styles.leaderPTtl}>Позиция</div> <div className={styles.leaderPTtl}>Пользователь</div>{" "}
+            <div className={styles.leaderPTtl}>Позиция</div> <div className={styles.leaderPTtl}>Пользователь</div>
             <div className={styles.leaderPTtl}>Время</div>
           </li>
-          <li className={styles.leader}>
-            <div className={styles.leaderP}>Позиция</div> <div className={styles.leaderP}>Пользователь</div>{" "}
-            <div className={styles.leaderP}>Время</div>
-          </li>
+          {leaders.map(leader => (
+            <li className={styles.leader} key={leader.id}>
+              <div className={styles.leaderP}>{leader.id}</div>
+              <div className={styles.leaderP}>{leader.name}</div>
+              <div className={styles.leaderP}>{leader.time}</div>
+            </li>
+          ))}
         </ul>
+        {error && <p> {error}</p>}
       </div>
     </div>
   );
